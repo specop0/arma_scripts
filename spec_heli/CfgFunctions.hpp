520ed4926bb122e070bf656fb9c90fb53e37ac5a
specop0 <specop0@yahoo.de>
2016-02-22 10:22:03 +0100

    spec_heli renamed function names to 'Spec_heli' and different names for MedEvac (capitalization of 'Evac' and 'Retransfer' without '_')

--- spec_heli/CfgFunctions.hpp
+++ spec_heli/CfgFunctions.hpp
@@ -1,10 +1,10 @@
 class spec_heli {
-	tag = "Spec";
+	tag = "Spec_heli";
 	class init {
 		file="spec_heli";
-		class heli_medevac {};
-		class heli_medevac_retransfer { postInit = 1; };
-		class heli_taxi {};
+		class medEvac {};
+		class medEvacRetransfer { postInit = 1; };
+		class taxi {};
 		class moveMarkerLZ { postInit = 1; };
 	};
 };
