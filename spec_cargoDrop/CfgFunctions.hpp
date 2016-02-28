9467d76ce0031879c283d29141dc9cef9797b34f
specop0 <specop0@yahoo.de>
2016-02-28 22:37:09 +0100

    spec_cargoDrop added: ACE Cargo droppable in flight with Parachute 1) in init of plane: [this] call Spec_cargoDrop_fnc_addParaDropEvent; - pilot and gunner (with EventHander GetIn) can drop ACE Cargo (interaction with plane) - dropped cargo will land safely (see addParachute) optional: small crates (NATO_Box_Base) can be packed into big supply crate ([player] call Spec_cargoDrop_fnc_addPackAction)

---
+++ spec_cargoDrop/CfgFunctions.hpp
@@ -0,0 +1,14 @@
+class spec_cargoDrop {
+	tag = "Spec_cargoDrop";
+	class init {
+		file="spec_cargoDrop";
+		class addPackAction {};
+		class addParachute {};
+		class addParadropEvent {};
+		class addUnloadActions {};
+		class addUnpackAction {};
+		class pack {};
+		class unload {};
+		class unpack {};
+	};
+};
---
+++ spec_cargoDrop/const.hpp
@@ -0,0 +1,22 @@
+#define ACE_CARGO_LOADED "ace_cargo_loaded"
+#define ACE_CARGO_SPACE "ace_cargo_space"
+
+#define SPEC_ATTACHED_CRATES_VAR "Spec_var_crateDrop_attachedCrates"
+
+#define SPEC_ACTION_UNLOAD_IDS "Spec_action_crateDrop_unload_%1"
+#define SPEC_ACTION_UNLOAD_DISTANCE_TO_PLANE 20.0
+
+#define SPEC_ACTION_UNPACK_ID "Spec_action_crateDrop_unpack"
+#define SPEC_ACTION_UNPACK_NAME "Kisten entpacken"
+
+#define SPEC_ACTION_PACK_ID "Spec_action_crateDrop_pack"
+#define SPEC_ACTION_PACK_NAME "Kisten packen"
+
+#define SPEC_ACTION_PACK_STATUS_TEXT "Verpacke Kiste %1/%2"
+#define SPEC_ACTION_PACK_ANIMATION "Acts_carFixingWheel"
+#define SPEC_ACTION_PACK_ANIMATION_TIME 12
+
+#define SPEC_ACTION_PACK_NO_CRATE_FOUND "Keine Kiste im Umkreis gefunden."
+
+#define CRATE_RADIUS_TO_SEARCH 7
+#define MAX_CRATES 4
